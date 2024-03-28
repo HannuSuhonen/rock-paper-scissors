05ee4319f238927f914cbe7be9cfd9145d7bc3ab
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-03-28 11:47:39 +0200

    Complete game functionality
    Update UI to follow player/cpu score

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
+let aiChoice;
 
 function getAIChoice(){
     let choices = ["rock","paper","scissors"];
     let randomNumber = Math.floor(Math.random() * 3); 
-    return choices[randomNumber];
-}
-
-function getPlayerChoice(){
-    let userChoice = prompt("Your choice?");
-    return userChoice
+    aiChoice = choices[randomNumber];
+    return aiChoice;
 }
 
 function playRound(playerselection, computerSelection){
@@ -17,57 +14,30 @@ function playRound(playerselection, computerSelection){
     let lowerCasePlayerChoice = playerselection.toLowerCase();
 
     if(lowerCasePlayerChoice == computerSelection){
-        return "Draw"
+        console.log("Draw");
     }else if(lowerCasePlayerChoice == "rock" && computerSelection == "scissors"){
         playerScore += 1;
-        return "You win!"
     }else if(lowerCasePlayerChoice == "rock" && computerSelection == "paper"){
         computerScore += 1;
-        return "You lose!"
     }else if(lowerCasePlayerChoice == "paper" && computerSelection == "scissors"){
         computerScore += 1;
-        return "You lose!"
     }else if(lowerCasePlayerChoice == "paper" && computerSelection == "rock"){
         playerScore += 1;
-        return "You win!"
     }else if(lowerCasePlayerChoice == "scissors" && computerSelection == "paper"){
         playerScore += 1;
-        return "You win!"
     }else if(lowerCasePlayerChoice == "scissors" && computerSelection == "rock"){
         computerScore +=1;
-        return "You lose!"
     }else{
-        return "Choice unrecognized!";
+        console.log("ERROR");
     }
 }
 
-function playGame(numberOfRounds = 3){
-    //Calls playround() until either player or computer has won the provided numberOfRounds (default 3)
-    while(playerScore < numberOfRounds && computerScore < numberOfRounds){
-        let result = playRound(getPlayerChoice(),getAIChoice())
-
-        if(result === null || result === undefined){
-            resetGame();
-            return;
-        }
-        alert(result);
-    }
-    endGame();
-}
-
-function resetGame(){
-    playerScore = 0;
-    computerScore = 0;
-}
-
-function endGame(){
-    playerScore > computerScore ? 
-    alert("Congratulations!, you won this best of 3") : 
-    alert("Dang, you lost this best of 3, better luck next time!") 
-    
-    if(confirm("Restart game?")){
-        resetGame();
-        playGame();
+function endGame(numberOfRounds = 5){
+    if(playerScore === numberOfRounds || computerScore === numberOfRounds){
+        const resultText = document.querySelector("h2");
+        playerScore > computerScore ? 
+        resultText.innerText = `Congratulations!, you won this best of ${numberOfRounds}` : 
+        resultText.innerText = `Dang, you lost this best of ${numberOfRounds}, better luck next time!`;
     }
 }
 
@@ -77,31 +47,26 @@ buttons.forEach((button) => {
     button.addEventListener("click", () => {
         switch(button.innerText){
             case "Rock":
-                console.log("Rock chosen");
                 playRound("Rock",getAIChoice());
                 break;
             case "Paper":
                 playRound("Paper",getAIChoice());
-
-                console.log("Paper chosen");
                 break;
             case "Scissors":
                 playRound("Scissors",getAIChoice());
-
-                console.log("Scissors chosen");
                 break;
             default:
                 console.log("Choice unidentified");
         }
         updateScore();
+        endGame(5);
     })
 });
 
-
-
 function updateScore(){
-    const results = document.querySelector(".score");
+    const texts = document.querySelector(".score").children;
 
-    results.firstElementChild.innerText = `player score: ${playerScore}`
-    results.lastElementChild.innerText = `cpu score: ${computerScore}`
+    texts.item(0).innerText = `ai choice: ${aiChoice}`
+    texts.item(1).innerText = `player score: ${playerScore}`
+    texts.item(2).innerText = `cpu score: ${computerScore}`
 }
