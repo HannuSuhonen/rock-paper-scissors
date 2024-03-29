065bc838c05d548b32242f658492504a3ecaadbe
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-03-29 17:30:11 +0200

    Update UI with images
    Change fonts and overall code cleanup.

--- myscripts.js
+++ myscripts.js
@@ -1,72 +1,84 @@
 let playerScore = 0;
 let computerScore = 0;
-let aiChoice;
 
 function getAIChoice(){
     let choices = ["rock","paper","scissors"];
     let randomNumber = Math.floor(Math.random() * 3); 
-    aiChoice = choices[randomNumber];
-    return aiChoice;
+    return choices[randomNumber];
 }
 
 function playRound(playerselection, computerSelection){
     if(playerselection === null || playerselection === undefined) return;
     let lowerCasePlayerChoice = playerselection.toLowerCase();
+    const roundText = document.querySelector(".round")
 
     if(lowerCasePlayerChoice == computerSelection){
-        console.log("Draw");
+        roundText.innerText = "Draw";
+        roundText.style.color = "gray"
+
     }else if(lowerCasePlayerChoice == "rock" && computerSelection == "scissors"){
         playerScore += 1;
+        roundText.innerText = `Win! AI chose ${computerSelection}`;
+        roundText.style.color = "green"
+
+        roundText.innerText.color
+
     }else if(lowerCasePlayerChoice == "rock" && computerSelection == "paper"){
         computerScore += 1;
+        roundText.innerText = `Lose! AI chose ${computerSelection}`;
+        roundText.style.color = "red"
+
+
     }else if(lowerCasePlayerChoice == "paper" && computerSelection == "scissors"){
         computerScore += 1;
+        roundText.innerText = `Lose! AI chose ${computerSelection}`;
+        roundText.style.color = "red"
+
+
     }else if(lowerCasePlayerChoice == "paper" && computerSelection == "rock"){
         playerScore += 1;
+        roundText.innerText = `Win! AI chose ${computerSelection}`;
+        roundText.style.color = "green"
+
+
     }else if(lowerCasePlayerChoice == "scissors" && computerSelection == "paper"){
         playerScore += 1;
+        roundText.innerText = `Win! AI chose ${computerSelection}`;
+        roundText.style.color = "green"
+
+
     }else if(lowerCasePlayerChoice == "scissors" && computerSelection == "rock"){
         computerScore +=1;
+        roundText.innerText = `Lose! AI chose ${computerSelection}`;
+        roundText.style.color = "red"
+
+
     }else{
         console.log("ERROR");
     }
 }
 
-function endGame(numberOfRounds = 5){
-    if(playerScore === numberOfRounds || computerScore === numberOfRounds){
-        const resultText = document.querySelector(".result");
-        playerScore > computerScore ? 
-        resultText.innerText = `Congratulations! You won this best of ${numberOfRounds}` : 
-        resultText.innerText = `Dang, you lost this best of ${numberOfRounds}, better luck next time!`;
-    }
-}
+// function endGame(numberOfRounds = 5){
+//     if(playerScore === numberOfRounds || computerScore === numberOfRounds){
+//         const resultText = document.querySelector(".result");
+//         playerScore > computerScore ? 
+//         resultText.innerText = `Congratulations! You won this best of ${numberOfRounds}` : 
+//         resultText.innerText = `Dang, you lost this best of ${numberOfRounds}, better luck next time!`;
+//     }
+// }
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("input");
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        switch(button.innerText){
-            case "Rock":
-                playRound("Rock",getAIChoice());
-                break;
-            case "Paper":
-                playRound("Paper",getAIChoice());
-                break;
-            case "Scissors":
-                playRound("Scissors",getAIChoice());
-                break;
-            default:
-                console.log("Choice unidentified");
-        }
+buttons.forEach((input) => {
+    input.addEventListener("click", () => {
+        playRound(input.className,getAIChoice());
         updateScore();
-        endGame(5);
+        //endGame(5);
     })
 });
 
 function updateScore(){
     const texts = document.querySelector(".score").children;
-
-    texts.item(0).innerText = `ai choice: ${aiChoice}`
-    texts.item(1).innerText = `player score: ${playerScore}`
-    texts.item(2).innerText = `cpu score: ${computerScore}`
+    texts.item(1).innerText = `Player score: ${playerScore}`
+    texts.item(2).innerText = `AI score: ${computerScore}`
 }
